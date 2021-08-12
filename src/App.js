eaa7fa964b9eba7ec8b5b11f64550e35543a7acc
czhc <cedriczhchan@gmail.com>
2021-08-12 12:53:26 +0800

    Setup multicall for uniswap default-token-list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEthers, useEtherBalance } from '@usedapp/core';
-import { formatEther } from '@ethersproject/units';
+import { useEthers, useEtherBalance, useContractCalls, ChainId, ERC20Interface } from '@usedapp/core';
+import { formatEther, formatUnits } from '@ethersproject/units';
 
+import uniswapToken from '@uniswap/default-token-list'
+
+const getTokenList = (chainId?: ChainId) =>
+    uniswapToken.tokens.filter((token) => token.chainId == chainId)
+
+const useTokensBalance = (tokenList: any[], account?: string | null) =>  {
+    return useContractCalls(
+      tokenList && account
+        ? tokenList.map((token: any) => ({
+          abi: ERC20Interface,
+          address: token.address,
+          method: 'balanceOf',
+          args: [account]
+        }))
+        : []
+      )
+}
 function App() {
 
   const {
@@ -14,24 +31,49 @@ function App() {
 
   const etherBalance = useEtherBalance(account);
 
-  // const tokenList = getTokenList(chainId);
-  // const balances = useTokensBalance(tokenList, account);
+  const tokenList = getTokenList(chainId);
+  const balances = useTokensBalance(tokenList, account);
 
   return (
     <div className="App">
+      <h1>useDapp Example</h1>
       <div><img src={logo} className="App-logo" alt="logo" /></div>
       <div>
         {
           account ?
-            <button onClick={() => deactivate()}>Disconnect</button> :
-            <button onClick={() => activateBrowserWallet()}>Connect</button>
+            <button class="App-Connect" onClick={() => deactivate()}>Disconnect</button> :
+            <button class="App-Connect" onClick={() => activateBrowserWallet()}>Connect</button>
         }
 
         { account && <p>Account: {account}</p>}
-        { etherBalance && <p>Balance: { formatEther(etherBalance) }</p>}
+        { etherBalance && <p>Balance: { formatEther(etherBalance) } ETH</p>}
+
+        <table class="App-Tokens--Table">
+          <tr>
+            <th></th>
+            <th>Symbol</th>
+            <th>Token</th>
+            <th>Balance</th>
+          </tr>
+          {
+            tokenList &&
+              tokenList.map((token, idx) => (
+                  <tr>
+                    <td><img class="App-Tokens--Icon" src={token.logoURI}/></td>
+                    <td><span class="App-Tokens--Symbol">{token.symbol}</span></td>
+                    <td><span class="App-Tokens--Name">{token.name}</span></td>
+                    <td><span>{
+                      balances[idx] &&
+                        formatUnits(balances[idx][0], token.decimals)}
+                    </span></td>
+                  </tr>
+                ))
+            }
+        </table>
       </div>
     </div>
   );
 }
+
 
 export default App;
